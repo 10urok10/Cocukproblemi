50c949886961013fdeb3f75b8ab05ee6f7a644fc
10urok10 <onrokten@gmail.com>
2025-05-26 22:59:25 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int erkek = 0;
+        int kadın = 0;
+
+        for (int i = 0; i < 100; i++)
+        {
+            int sayi = rand.Next(0, 2);
+
+            if (sayi == 0)
+            {
+                erkek++;
+            }
+            else
+            {
+                while(sayi == 1)
+                {
+                    kadın++;
+                    sayi = rand.Next(0, 2);
+                }
+                erkek++;
+
+            }
+
+        }
+        Console.WriteLine("erkek sayisi = " + erkek);
+        Console.WriteLine("kadin sayisi =" + kadın);
+    }
+}
